9c10f45bd8b4805255a197eb6aca2b0e4956003f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-12-28 18:24:03 +0100

    Fix build.gradle setup and remove render type registration in AdditionalLanternsClient

--- src/main/java/com/supermartijn642/additionallanterns/AdditionalLanternsClient.java
+++ src/main/java/com/supermartijn642/additionallanterns/AdditionalLanternsClient.java
@@ -1,22 +1,10 @@
 package com.supermartijn642.additionallanterns;
 
-import com.supermartijn642.core.registry.ClientRegistrationHandler;
-
 /**
  * Created 7/1/2021 by SuperMartijn642
  */
 public class AdditionalLanternsClient {
 
     public static void register(){
-        ClientRegistrationHandler handler = ClientRegistrationHandler.get("additionallanterns");
-        for(LanternMaterial material : LanternMaterial.values()){
-            handler.registerBlockModelCutoutRenderType(material::getLanternBlock);
-            if(material.canBeColored){
-                for(LanternColor color : LanternColor.values())
-                    handler.registerBlockModelCutoutRenderType(() -> material.getLanternBlock(color));
-            }
-            if(material.hasChains)
-                handler.registerBlockModelCutoutRenderType(material::getChainBlock);
-        }
     }
 }
