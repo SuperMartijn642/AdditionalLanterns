43840edcdbd7a84f9737303250ff7fbb0bf6491b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-07-31 23:28:31 +0200

    Add new lantern materials

--- src/main/java/com/supermartijn642/additionallanterns/AdditionalLanterns.java
+++ src/main/java/com/supermartijn642/additionallanterns/AdditionalLanterns.java
@@ -5,6 +5,7 @@ import com.supermartijn642.core.item.CreativeItemGroup;
 import com.supermartijn642.core.registry.GeneratorRegistrationHandler;
 import com.supermartijn642.core.registry.RegistrationHandler;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.OxidizableBlocksRegistry;
 
 /**
  * Created 7/7/2020 by SuperMartijn642
@@ -27,6 +28,39 @@ public class AdditionalLanterns implements ModInitializer {
             handler.registerBlockCallback(material::registerBlocks);
             handler.registerItemCallback(material::registerItems);
         }
+        handler.registerBlockCallback(helper -> {
+            // Waxing
+            OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.COPPER.getLanternBlock(), LanternMaterial.WAXED_COPPER.getLanternBlock());
+            OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.COPPER.getChainBlock(), LanternMaterial.WAXED_COPPER.getChainBlock());
+            OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.EXPOSED_COPPER.getLanternBlock(), LanternMaterial.WAXED_EXPOSED_COPPER.getLanternBlock());
+            OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.EXPOSED_COPPER.getChainBlock(), LanternMaterial.WAXED_EXPOSED_COPPER.getChainBlock());
+            OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.WEATHERED_COPPER.getLanternBlock(), LanternMaterial.WAXED_WEATHERED_COPPER.getLanternBlock());
+            OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.WEATHERED_COPPER.getChainBlock(), LanternMaterial.WAXED_WEATHERED_COPPER.getChainBlock());
+            OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.OXIDIZED_COPPER.getLanternBlock(), LanternMaterial.WAXED_OXIDIZED_COPPER.getLanternBlock());
+            OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.OXIDIZED_COPPER.getChainBlock(), LanternMaterial.WAXED_OXIDIZED_COPPER.getChainBlock());
+            for(LanternColor color : LanternColor.values()){
+                OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.COPPER.getLanternBlock(color), LanternMaterial.WAXED_COPPER.getLanternBlock(color));
+                OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.EXPOSED_COPPER.getLanternBlock(color), LanternMaterial.WAXED_EXPOSED_COPPER.getLanternBlock(color));
+                OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.WEATHERED_COPPER.getLanternBlock(color), LanternMaterial.WAXED_WEATHERED_COPPER.getLanternBlock(color));
+                OxidizableBlocksRegistry.registerWaxableBlockPair(LanternMaterial.OXIDIZED_COPPER.getLanternBlock(color), LanternMaterial.WAXED_OXIDIZED_COPPER.getLanternBlock(color));
+            }
+
+            // Weathering
+            OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.COPPER.getLanternBlock(), LanternMaterial.EXPOSED_COPPER.getLanternBlock());
+            OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.EXPOSED_COPPER.getLanternBlock(), LanternMaterial.WEATHERED_COPPER.getLanternBlock());
+            OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.WEATHERED_COPPER.getLanternBlock(), LanternMaterial.OXIDIZED_COPPER.getLanternBlock());
+            OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.COPPER.getChainBlock(), LanternMaterial.EXPOSED_COPPER.getChainBlock());
+            OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.EXPOSED_COPPER.getChainBlock(), LanternMaterial.WEATHERED_COPPER.getChainBlock());
+            OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.WEATHERED_COPPER.getChainBlock(), LanternMaterial.OXIDIZED_COPPER.getChainBlock());
+            for(LanternColor color : LanternColor.values()){
+                OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.COPPER.getLanternBlock(color), LanternMaterial.EXPOSED_COPPER.getLanternBlock(color));
+                OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.EXPOSED_COPPER.getLanternBlock(color), LanternMaterial.WEATHERED_COPPER.getLanternBlock(color));
+                OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.WEATHERED_COPPER.getLanternBlock(color), LanternMaterial.OXIDIZED_COPPER.getLanternBlock(color));
+                OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.COPPER.getChainBlock(), LanternMaterial.EXPOSED_COPPER.getChainBlock());
+                OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.EXPOSED_COPPER.getChainBlock(), LanternMaterial.WEATHERED_COPPER.getChainBlock());
+                OxidizableBlocksRegistry.registerOxidizableBlockPair(LanternMaterial.WEATHERED_COPPER.getChainBlock(), LanternMaterial.OXIDIZED_COPPER.getChainBlock());
+            }
+        });
     }
 
     private static void registerGenerators(){
