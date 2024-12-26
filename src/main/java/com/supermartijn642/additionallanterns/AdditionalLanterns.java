521e3360d722d868ba5342e017f88241648fe201
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-12-26 16:46:38 +0100

    Update to 1.21.4

--- src/main/java/com/supermartijn642/additionallanterns/AdditionalLanterns.java
+++ src/main/java/com/supermartijn642/additionallanterns/AdditionalLanterns.java
@@ -72,6 +72,7 @@ public class AdditionalLanterns implements ModInitializer {
     private static void registerGenerators(){
         GeneratorRegistrationHandler handler = GeneratorRegistrationHandler.get("additionallanterns");
         handler.addGenerator(LanternBlockModelGenerator::new);
+        handler.addGenerator(LanternItemInfoGenerator::new);
         handler.addGenerator(LanternItemModelGenerator::new);
         handler.addGenerator(LanternBlockStateGenerator::new);
         handler.addGenerator(LanternLanguageGenerator::new);
