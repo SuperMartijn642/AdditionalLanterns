d58d30cdca9f9e830abbb7af078538b62b507c9c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-14 23:01:54 +0200

    Don't replace vanilla lantern item registry entry

--- src/main/java/com/supermartijn642/additionallanterns/LanternBlock.java
+++ src/main/java/com/supermartijn642/additionallanterns/LanternBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -84,5 +85,12 @@ public class LanternBlock extends net.minecraft.world.level.block.LanternBlock {
 
     public static boolean emitsLight(BlockState state){
         return state.getValue(REDSTONE) != state.getValue(ON);
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(LevelReader level, BlockPos pos, BlockState state){
+        if(this.material == LanternMaterial.NORMAL && this.color == null)
+            return new ItemStack(Blocks.LANTERN);
+        return super.getCloneItemStack(level, pos, state);
     }
 }
