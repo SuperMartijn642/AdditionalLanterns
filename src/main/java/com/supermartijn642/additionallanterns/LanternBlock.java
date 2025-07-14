66a03832a574e4797453f6a57429ce7e34b02c19
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-14 23:21:23 +0200

    Don't replace vanilla lantern item registry entry

--- src/main/java/com/supermartijn642/additionallanterns/LanternBlock.java
+++ src/main/java/com/supermartijn642/additionallanterns/LanternBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -16,6 +17,7 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.redstone.Orientation;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.Nullable;
 
 
@@ -85,5 +87,12 @@ public class LanternBlock extends net.minecraft.world.level.block.LanternBlock {
 
     public static boolean emitsLight(BlockState state){
         return state.getValue(REDSTONE) != state.getValue(ON);
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(BlockState state, HitResult target, LevelReader level, BlockPos pos, Player player){
+        if(this.material == LanternMaterial.NORMAL && this.color == null)
+            return new ItemStack(Blocks.LANTERN);
+        return super.getCloneItemStack(state, target, level, pos, player);
     }
 }
